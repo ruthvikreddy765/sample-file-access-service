17373956a84dbded707bf2883d90521d00950b68
Ruthvik Reddy <ruthvikreddy@Mac.hsd1.ga.comcast.net>
2026-07-05 12:58:13 -0400

    Remove node_modules and dist, add gitignore

--- dist/src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = process.env.PORT || 3000;
-const app = (0, app_1.createApp)();
-app.listen(PORT, () => {
-    console.log(`Sample File Access Service running on http://localhost:${PORT}`);
-});
